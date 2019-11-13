2279f45224c8a35400b9ebafbc3e7800ea8350ca
zachyutw1117 <zachyu.tw@gmail.com>
2019-11-12 20:10:30 -0800

    change setting

--- API/auth.firebase.api.js
+++ API/auth.firebase.api.js
@@ -7,47 +7,17 @@ import { GoogleSignin } from '@react-native-community/google-signin';
 import { pick } from 'lodash';
 import axios from 'axios';
 import sleep from 'sleep-promise';
-// export const firebase = firebaseApp.initializeApp({
-//     apiKey: 'AIzaSyDBxx4lyOoBJDpKlLS7ye_IomI1O4UVsME',
-//     authDomain: 'project-id.firebaseapp.com',
-//     appID: '1:571377425932:web:f7a9159544dcfbffc2d829',
-//     storageBucket: 'gs://soy-haven-237204.appspot.com/images'
-// });
+
 firebase.auth().settings.appVerificationDisabledForTesting = true;
 const phoneNumber = '+16505554567';
 const testVerificationCode = '123456';
-// const appVerifier = new firebaseApp.auth.RecaptchaVerifier('root');
-// const googleProvider = new firebase.auth.GoogleAuthProvider();
-// googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-// googleProvider.setCustomParameters({
-//     login_hint: 'user@example.com'
-// });
-
-// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-//     .then(function () {
-//         // Existing and future Auth states are now persisted in the current
-//         // session only. Closing the window would clear any existing state even
-//         // if a user forgets to sign out.
-//         // ...
-//         // New sign-in will be persisted with session persistence.
-//         return firebase.auth().signInWithEmailAndPassword(email, password);
-//     })
-//     .catch(function (error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//     });
-
-// firebase.auth().currentUser.getIdToken(true).then((idTokern) => {
-//     console.log(idTokern);
-// });
 
 const USER_PICK_FIELDS = [ 'email', 'displayName', 'phoneNumber', 'photoURL' ];
 
 export const signInWithGoolge = async () => {
     await GoogleSignin.configure({
         scopes: [ 'https://www.googleapis.com/auth/drive.readonly' ],
-        webClientId: '571377425932-s5q178astnv9rvu1ka876hrr6c5ej0k5.apps.googleusercontent.com' // required
+        webClientId: process.env.REACT_NATIVE_APP_FIREBASE_WEB_CLIENT_ID
     });
     const { accessToken, idToken } = await GoogleSignin.signIn();
     const credential = firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
